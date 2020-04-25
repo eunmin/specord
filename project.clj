cfc5eab234235d045512f7d32ec41bcb3e1d00f3
Eunmin Kim <telepopsound@gmail.com>
2020-04-26 00:24:28 +0900

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject specord "0.1.0-SNAPSHOT"
+(defproject specord "0.1.0"
   :description "Define record with spec"
   :url "https://github.com/eunmin/specord"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
